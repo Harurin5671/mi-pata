38bb0a6437229f0580e0ec9a1cf04be9f66f2586
Harurin5671 <frankcito639@gmail.com>
2024-07-05 00:35:51 -0500

    Agregando mas screens a las rutas

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import {
-  createNativeStackNavigator
-} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { type RootStackParamList } from '@interfaces/navigation'
 
-import HomeScreen from '@screens/HomeScreen'
+import AuthScreen from '@screens/AuthScreen'
+import WelcomeScreen from '@screens/WelcomeScreen'
+import SignUpScreen from '@screens/SignUpScreen'
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const AppNavigator = (): React.ReactElement => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Navigator initialRouteName="AuthScreen">
         <Stack.Screen
-          name="HomeScreen"
-          component={HomeScreen}
+          name="AuthScreen"
+          component={AuthScreen}
           options={{ headerShown: false }}
         />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="SignUpScreen"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
